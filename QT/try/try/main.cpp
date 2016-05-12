32d749a74d0f5c81e1ce48abd5b30aa3df1253a7
mrfreemen <m-freemen@live.com>
2016-05-12 21:52:02 +0300

    ???

--- QT/try/try/main.cpp
+++ QT/try/try/main.cpp
@@ -1,11 +1,9 @@
-#include "mainwindow.h"
 #include <QApplication>
+#include <QLabel>
 
-int main(int argc, char *argv[])
-{
-    QApplication a(argc, argv);
-    MainWindow w;
-    w.show();
-
-    return a.exec();
+int main(int argc,char *argv[]){
+  QApplication qap(argc,argv);
+  QLabel qlab("Hello world!");
+  qlab.show();
+  return qap.exec();
 }
